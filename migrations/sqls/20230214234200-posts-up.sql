c2d4f2e6e1f78f2789083454914662179b4029dc
leowyatt <leowyatt@yandex.ru>
2023-02-18 21:40:11 +0300

    init

--- migrations/sqls/20230214234200-posts-up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.posts (
-    id uuid DEFAULT uuid_generate_v4(),
-    title text,
-    PRIMARY KEY (id)
-);
-ALTER TABLE IF EXISTS public.posts OWNER to db;
-GRANT ALL ON TABLE public.posts TO "GUEST";
